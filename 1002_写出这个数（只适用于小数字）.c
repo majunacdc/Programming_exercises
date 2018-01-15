578e36aab4fcfb73c7857b29c40aa91b4076e1d5
Joe Ma <majunacdc@outlook.com>
2018-01-15 16:39:55 +0800

    下次换一种

---
+++ 1002_写出这个数（只适用于小数字）.c
@@ -0,0 +1,44 @@
+/********只适用于小数字（十位数）*******/
+#include<stdio.h>
+int main()
+{
+    char *A[10]={"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
+    int  B[100]={};
+    int i,j;
+    int sum=0;
+    int add=0;
+    int t=0;
+    scanf("%d",&i);
+
+    for(i;i!=0;i=i/10)
+   {
+        sum+=i%10;
+    }
+   printf("%d\n",sum);
+   add=sum;
+   for(sum;sum!=0;sum=sum/10)
+   {
+       t++;
+   }
+
+   for(j=0;j<t;j++)
+   {
+       B[j]=add%10;
+       add=add/10;
+   }
+
+
+  /*for(i=0;i<t;i++)
+   {
+    printf("%d",B[t-i-1]);
+   }
+  */
+   for(i=0;i<t;i++)
+   {
+    printf("%s",A[B[t-i-1]]);
+    if(i!=t-1)
+        printf(" ");
+   }
+
+return 0;
+}
